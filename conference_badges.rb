1ebb0a889c2a81cd4fa3613af4c11e686a5bd9df
Joshua Ortega <joshua.ortega13@gmail.com>
2020-06-10 18:39:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,23 +1,5 @@
-def conference_badges
-
-
-end
-
 def badge_maker(name)
   puts "Hello, my name is #{name}."
-return
-end
-
-def batch_badge_creator
-
-
-end
-def assign_rooms
-
-
-end
-def printer
-puts line.chomp
 
 
 end
