dc4f6bf488ff434f200947d71f953aa803bee67d
Joshua Ortega <joshua.ortega13@gmail.com>
2020-06-10 18:45:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,13 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-
+  ["Hello, my name is Edsger.",
+  "Hello, my name is Ada.",
+  "Hello, my name is Charles.",
+  "Hello, my name is Alan.",
+  "Hello, my name is Grace.",
+  "Hello, my name is Linus.",
+  "Hello, my name is Matz."
+]
 
 end
